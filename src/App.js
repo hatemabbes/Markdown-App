9ddc297facebe43507c75dd1f8e0462805d70e9d
Hatem <hatem.aabbes@gmail.com>
2021-03-04 13:09:39 +0100

    initialised app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="container">
+          <div className="row">
+          <div className="col-sm-6">
+              <textarea value="test me" rows="35"/>
+          </div>
+          <div className="col-sm-6">
+            Résultat
+          </div>
+          </div>
+          
+        </div>
+      </Fragment>
+
+    );
+  }
+
 }
 
 export default App;
