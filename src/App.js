9bafe2df2221c2ae086fbfc8d2cbbe75d0c0681b
Hatem <hatem.aabbes@gmail.com>
2021-03-04 13:15:37 +0100

    handle change event and synchronize with state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component, Fragment } from 'react';
+import { sampleText } from './sampleText'
 import './App.css';
+import marked from 'marked'
 
 class App extends Component {
+  state = {
+    text: sampleText
+  }
+  handlechange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
   render() {
     return (
       <Fragment>
         <div className="container">
           <div className="row">
-          <div className="col-sm-6">
-              <textarea value="test me" rows="35"/>
+            <div className="col-sm-6">
+              <textarea value={this.state.text} onChange={this.handlechange} className="form-control" rows="35" />
+            </div>
+            <div className="col-sm-6">
+              {this.state.text}
+            </div>
           </div>
-          <div className="col-sm-6">
-            Résultat
-          </div>
-          </div>
-          
+
         </div>
       </Fragment>
 
